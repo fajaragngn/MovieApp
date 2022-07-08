5b261de1043b3c843a23d371636e1d51d10cb7bd
fajaragngn <fajaragngn@gmail.com>
2022-07-09 06:00:52 +0700

    Styling inner title 🍰

--- src/component/footer.js
+++ src/component/footer.js
@@ -8,9 +8,6 @@ function Footer(){
 <a href="https://developers.themoviedb.org/3/getting-started/introduction" target="_blank">
 <img src={logo} alt="" />
 </a>
-<a href="https://github.com/bay-s/bay-s.github.io" target="_blank">
-<img src={github} className="git" alt="" />
-</a>
 </div>
 </footer>
     )
